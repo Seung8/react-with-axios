ff8b6a4b70bd57be783044c7bc2fb6ff5964f379
Seungyun Lee <lsy0914@naver.com>
2020-11-13 18:45:53 +0900

    - API 요청 상태 reducer 이용

--- src/Users.js
+++ src/Users.js
@@ -1,36 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
+// Loading, Success, Error 상태 관리
+function reducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case 'SUCCESS':
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case 'ERROR':
+      return {
+        loading: false,
+        data: null,
+        error: null,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
 
   // API 요청 로직
   const fetchUsers = async () => {
-    try {
-      // 초기화
-      setUsers(null);
-      setError(null);
-      setLoading(true);
+    dispatch({ type: 'LOADING' });
 
-      // API 요청
+    try {
       const response = await axios.get(
         'https://jsonplaceholder.typicode.com/users/',
       );
 
-      // 응답 데이터(response.data) users로 할당
-      setUsers(response.data);
+      dispatch({ type: 'SUCCESS', data: response.data });
     } catch (e) {
-      setError(e);
+      dispatch({ type: 'ERROR', error: e });
     }
-    setLoading(false);
   };
 
   // 최초 렌더링될 때 API 요청. deps가 빈 배열이면 최초 레더링될 때에만 동작
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  const { loading, data: users, error } = state;
 
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러 발생</div>;
@@ -41,11 +65,11 @@ function Users() {
       <ul>
         {users.map((user) => (
           <li key={user.id}>
-            {user.username} ({user.name})
+            {user.name}({user.email})
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>데이터 불러오기</button>
+      <button onClick={fetchUsers}>재요청</button>
     </>
   );
 }
