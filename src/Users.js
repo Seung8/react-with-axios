5c546bc2b82c508df1bd7db861ee6b4052899bfa
Seungyun Lee <lsy0914@naver.com>
2020-11-22 16:58:39 +0900

    - useAsync 커스텀 훅 수정

--- src/Users.js
+++ src/Users.js
@@ -1,59 +1,16 @@
-import React, { useEffect, useReducer } from 'react';
+import React from 'react';
 import axios from 'axios';
+import useAsync from './useAsync';
 
-// Loading, Success, Error 상태 관리
-function reducer(state, action) {
-  switch (action.type) {
-    case 'LOADING':
-      return {
-        loading: true,
-        data: null,
-        error: null,
-      };
-    case 'SUCCESS':
-      return {
-        loading: false,
-        data: action.data,
-        error: null,
-      };
-    case 'ERROR':
-      return {
-        loading: false,
-        data: null,
-        error: null,
-      };
-    default:
-      throw new Error(`Unhandled action type: ${action.type}`);
-  }
+async function getUsers() {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/users/',
+  );
+  return response.data;
 }
 
 function Users() {
-  const [state, dispatch] = useReducer(reducer, {
-    loading: false,
-    data: null,
-    error: null,
-  });
-
-  // API 요청 로직
-  const fetchUsers = async () => {
-    dispatch({ type: 'LOADING' });
-
-    try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/users/',
-      );
-
-      dispatch({ type: 'SUCCESS', data: response.data });
-    } catch (e) {
-      dispatch({ type: 'ERROR', error: e });
-    }
-  };
-
-  // 최초 렌더링될 때 API 요청. deps가 빈 배열이면 최초 레더링될 때에만 동작
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
+  const [state, refetch] = useAsync(getUsers);
   const { loading, data: users, error } = state;
 
   if (loading) return <div>로딩중..</div>;
@@ -69,7 +26,7 @@ function Users() {
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>재요청</button>
+      <button onClick={refetch}>재요청</button>
     </>
   );
 }
---
+++ src/useAsync.js
@@ -0,0 +1,52 @@
+import { useReducer, useEffect, useCallback } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case 'SUCCESS':
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case 'ERROR':
+      return {
+        loading: false,
+        data: null,
+        error: null,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
+// callback : API 호출 함수
+// deps: 컴포넌트가 로딩됐을 때, 값이 변경됐을 때 useEffect의 deps
+export default function useAsync(callback, deps = []) {
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
+
+  const fetchData = async () => {
+    dispatch({ type: 'LOADING' });
+    try {
+      const data = await callback();
+      dispatch({ type: 'SUCCESS', data });
+    } catch (e) {
+      dispatch({ type: 'ERROR', error: e });
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, deps);
+
+  return [state, fetchData];
+}
